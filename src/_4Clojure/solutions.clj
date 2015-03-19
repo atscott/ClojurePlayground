e217ac2cc2fce1d69d0c8d26b633aee3c9fa3bef
AScott <atscott01@gmail.com>
2015-03-19 15:20:12 -0500

    29

--- src/_4Clojure/solutions.clj
+++ src/_4Clojure/solutions.clj
@@ -4,28 +4,28 @@
 
 (defn number25 [x] (filter odd? x))
 
-(defn number26
+(defn number26 [n]
   "Write a function which returns the first X fibonacci numbers."
-  [n]
   (loop [n n, last 0, current 1, acc '()]
     (if (< n 1)
       acc
-      (recur (dec n) current (+ current last) (concat acc `(~current)))))
-  )
+      (recur (dec n) current (+ current last) (concat acc `(~current))))))
 
-(defn number27
+(defn number27 [x]
   "Write a function which returns true if the given sequence is a palindrome."
-  [x]
   (if (< (count x) 2)
     true
     (if (= (first x) (last x))
       (recur (drop-last (rest x)))
       false)))
 
-(defn number28
+(defn number28 [xs]
   "Write a function which flattens a sequence."
-  [xs]
-    (let [x (first xs) ys (rest xs)]
-      (concat
-        (if (coll? x) (number28 x) [x])
-        (when (not-empty ys) (number28 ys)))))
+  (let [x (first xs) ys (rest xs)]
+    (concat
+      (if (coll? x) (number28 x) [x])
+      (when (not-empty ys) (number28 ys)))))
+
+(defn number29 [s]
+  "Write a function which takes a string and returns a new string containing only the capital letters."
+  (apply str (re-seq #"[A-Z]" s)))
