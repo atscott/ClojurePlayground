027bc2a6ec2105ba19f8dac0e81c2d59d242c76f
atscott <atscott01@gmail.com>
2015-03-18 19:12:20 -0500

    doing stuff

---
+++ src/_4Clojure/solutions.clj
@@ -0,0 +1,23 @@
+; http://www.4clojure.com/problem/[number]
+
+(defn number24 [x] (reduce + x))
+
+(defn number25 [x] (filter odd? x))
+
+(defn number26
+  "Write a function which returns the first X fibonacci numbers."
+  [n]
+  (loop [n n, last 0, current 1, acc '()]
+    (if (< n 1)
+      acc
+      (recur (dec n) current (+ current last) (concat acc `(~current)))))
+  )
+
+(defn number27
+  "Write a function which returns true if the given sequence is a palindrome."
+  [x]
+  (if (< (count x) 2)
+    true
+    (if (= (first x) (last x))
+      (recur (drop-last (rest x)))
+      false)))
