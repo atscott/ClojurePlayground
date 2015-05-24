4a2fe9b51abea99d34cd5370720eda2860172a69
atscott <atscott01@gmail.com>
2015-05-24 16:56:43 -0500

    playing around

--- src/longest-increasing-sequence.clj
+++ src/longest-increasing-sequence.clj
@@ -3,20 +3,25 @@
     (filter #(get-in grid %) ns)))
 
 (def memo-increasing #(0))
-(defn find-max [x y grid]
+(defn increasing [x y grid]
   (let [ns (get-neighbors x y grid)
         increasing-neighbors (filter #(> (get-in grid %) (get-in grid [x y])) ns)]
     (if (empty? increasing-neighbors)
       1
       (apply max (map #(inc (memo-increasing (first %) (last %) grid)) increasing-neighbors)))))
-(def memo-increasing (memoize find-max))
+(def memo-increasing (memoize increasing))
 
-(defn longest-increasing [grid]
-  (let [depths (for [x (range 0 (count grid))
-                     y (range 0 (count (first grid)))]
-                 (memo-increasing x y grid))]
-    (apply max depths)))
+(defn find-increasing [grid]
+  (apply max (for [x (range 0 (count grid))
+                   y (range 0 (count (first grid)))]
+               (memo-increasing x y grid))))
 
-(longest-increasing [[1 2 2 4]
-                     [4 5 4 1]
-                     [7 2 6 7]])
+(find-increasing [[14 13 21 32 38]
+                  [95 14 55 88 88]
+                  [12 32 56 17 14]
+                  [19 90 10 77 58]
+                  [44 98 19 54 16]])
+
+(find-increasing [[1 2 8 4]
+                  [5 6 8 8]
+                  [9 1 4 3]])
