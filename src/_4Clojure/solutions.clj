7809d9eb9c5cd9b2274abd18f0c694bc60266e0e
atscott <atscott01@gmail.com>
2015-06-27 09:43:47 -0500

    format

--- src/_4Clojure/solutions.clj
+++ src/_4Clojure/solutions.clj
@@ -399,11 +399,10 @@
 (defn number110
   "Write a function that returns a lazy sequence of 'pronunciations' of a sequence of numbers. A pronunciation of each element in the sequence consists of the number of repeating identical numbers and the number itself. For example, [1 1] is pronounced as [2 1] ('two ones'), which in turn is pronounced as [1 2 1 1] ('one two, one one')."
   {:test (fn []
-           (is (= [[1 1] [2 1] [1 2 1 1]] (take 3 ( number110 [1]))))
+           (is (= [[1 1] [2 1] [1 2 1 1]] (take 3 (number110 [1]))))
            (is (= [3 1 2 4] (first (number110 [1 1 1 4 4]))))
            (is (= [1 1 1 3 2 1 3 2 1 1] (nth (number110 [1]) 6)))
-           (is (= 338 (count (nth (number110 [3 2]) 15)))))
-           }
+           (is (= 338 (count (nth (number110 [3 2]) 15)))))}
   [xs]
   (let [parts (partition-by identity xs)
         pronunciation (mapcat #(vector (count %) (first %)) parts)]
