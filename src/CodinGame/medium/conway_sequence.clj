0163bc81cc4c68890b99fd8c0fd0e429ae255685
atscott <atscott01@gmail.com>
2015-07-14 21:30:00 -0700

    formatting

--- src/CodinGame/medium/conway_sequence.clj
+++ src/CodinGame/medium/conway_sequence.clj
@@ -4,14 +4,15 @@
 (use '[clojure.test :as t])
 
 (defn conway
+  "only returns the requested line for the given seed"
   {:test (fn []
            (is (= [1 1 1 3 1 2 2 1 1 3 3 1 1 2 1 3 2 1 1 3 2 1 2 2 2 1] (conway 1 11)))
            (is (= [2] (conway 2 1)))
            (is (= [3 1 1 3 1 1 2 2 2 1 1 3 1 1 1 2 3 1 1 3 3 2 2 1 1 5] (conway 5 10)))
            (is (= [3 1 1 3 1 1 2 2 2 1 1 3 1 1 1 2 3 1 1 3 3 2 2 1 1 25] (conway 25 10))))}
-  [R L]
-  (loop [previous-line [R] i 1]
-    (if (= i L)
+  [seed line]
+  (loop [previous-line [seed] i 1]
+    (if (= i line)
       (do (->> (interpose " " previous-line)
                (apply str)
                (println))
@@ -21,16 +22,16 @@
         (recur next-line (inc i))))))
 
 (defn conway-2
+  "returns lazy sequence"
   {:test (fn []
            (is (= [1 1 1 3 1 2 2 1 1 3 3 1 1 2 1 3 2 1 1 3 2 1 2 2 2 1] (nth (conway-2 1) 11)))
            (is (= [2] (first (conway-2 2))))
            (is (= [3 1 1 3 1 1 2 2 2 1 1 3 1 1 1 2 3 1 1 3 3 2 2 1 1 5] (nth (conway-2 5) 10)))
            (is (= [3 1 1 3 1 1 2 2 2 1 1 3 1 1 1 2 3 1 1 3 3 2 2 1 1 25] (nth (conway-2 25) 10))))}
   [seed]
-  (cons [seed]
-        (iterate #(->> (partition-by identity %)
-                       (mapcat (juxt count first)))
-                 [seed])))
+  (cons [seed] (iterate #(->> (partition-by identity %)
+                              (mapcat (juxt count first)))
+                        [seed])))
 
 (defn -main [& args]
   (let [start (read) line (read)]
