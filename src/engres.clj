882a258ee3a66f9abaaaddd446bcd4433aee2087
atscott <atscott@atscott-macbookpro.roam.corp.google.com>
2015-08-27 10:57:09 -0700

    conway

--- src/engres.clj
+++ src/engres.clj
@@ -12,9 +12,15 @@
                    [1 4 6 4 1]] (take 5 (secret-function))))
            (is (= [1 10 45 120 210 252 210 120 45 10 1] (nth (secret-function) 10))))}
   []
+  (let [zip-with (fn [f & colls]
+                   (->> (apply interleave colls)
+                        (partition (count colls))
+                        (map #(apply f %))))]
   (iterate #(concat [1]
-                    (map + % (rest %))
+                    ;(map + % (rest %))
+                    (zip-with + % (rest %))
                     [1])
-           '(1)))
+           '(1))))
 
 (t/run-tests)
+
